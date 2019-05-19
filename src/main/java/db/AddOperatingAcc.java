eae647271d53d8b64d5ede1ff4196989ff82e977
EvgeniyaDod <dodonova.evg@yandex.ru>
2019-05-19 21:56:53 +0400

    Поправила организации при добавлении р/с. Защита от дураков при пустых полях в добавлениях

--- src/main/java/db/AddOperatingAcc.java
+++ src/main/java/db/AddOperatingAcc.java
@@ -1,6 +1,8 @@
 package db;
 
 import javax.swing.*;
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class AddOperatingAcc extends JFrame {
@@ -24,17 +27,33 @@ public class AddOperatingAcc extends JFrame {
     private DatabaseConnection mdbc;
     private Statement stmt;
 
-    public AddOperatingAcc(final String persNum){
+    public AddOperatingAcc(final String persNum, JTable b, JTable p, JTable c){
         setContentPane(rootPanel);
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         setLocation(dimension.width/2-this.getSize().width/2,dimension.height/2-this.getSize().height/2);
         panelDateStart.add(dcDateStart);
+        dcDateStart.setFont(new Font("Microsoft Sans Serif", Font.PLAIN, 18));
+        getComboBoxOrganization(textOrgComboBox, b, p, c);
+        setMasks();
+
         saveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                addOperAcc(persNum);
-                JOptionPane.showMessageDialog(AddOperatingAcc.this,"Успешно добавлено","Добавление",JOptionPane.INFORMATION_MESSAGE);
-                setVisible(false);
-                dispose();
+                setNullBorder();
+                if(textNumber.getText().equals("********************")) {
+                    textNumber.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.RED));
+                    showMess();
+                }else if(dcDateStart.getDate()==null) {
+                    dcDateStart.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.RED));
+                    showMess();
+                }else {
+                    int rez = addOperAcc(persNum);
+                    if (rez == 1)
+                        JOptionPane.showMessageDialog(AddOperatingAcc.this, "Успешно добавлено!", "Добавление", JOptionPane.INFORMATION_MESSAGE);
+                    else
+                        JOptionPane.showMessageDialog(AddOperatingAcc.this, "Ошибка при добавлении", "Добавление", JOptionPane.WARNING_MESSAGE);
+                    setVisible(false);
+                    dispose();
+                }
             }
         });
         canselButton.addActionListener(new ActionListener() {
@@ -47,12 +66,36 @@ public class AddOperatingAcc extends JFrame {
             }
         });
     }
-    private void addOperAcc(String personalNumber){
+
+    private void setNullBorder(){
+        textNumber.setBorder(BorderFactory.createMatteBorder(1,1,1,1, Color.GRAY));
+        dcDateStart.setBorder(BorderFactory.createMatteBorder(1,1,1,1, Color.GRAY));
+    }
+    private void showMess(){
+        JOptionPane.showMessageDialog(AddOperatingAcc.this, "Должны быть введены все поля", "Добавление", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void setMasks(){
+        try {
+            MaskFormatter numberDocFormatter = new MaskFormatter("####################");
+            numberDocFormatter.setPlaceholderCharacter('*');
+            DefaultFormatterFactory numberDocFactory = new DefaultFormatterFactory(numberDocFormatter);
+
+            textNumber.setFormatterFactory(numberDocFactory);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int addOperAcc(String personalNumber){
         String number = textNumber.getText();
 //        String dateStart = textDateStart.getText();
         String status = "Действителен";
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String dateStart = dateFormat.format(dcDateStart.getDate());
+        String org = textOrgComboBox.getItemAt(textOrgComboBox.getSelectedIndex()).toString();
+        int numLine = org.indexOf("|");
+        String numOrg=org.substring(0, numLine-1);
 
         try {
             mdbc = new DatabaseConnection();
@@ -63,46 +106,59 @@ public class AddOperatingAcc extends JFrame {
                     "("+quotate(number)+","+quotate(dateStart)+","+quotate(status)+","+quotate(personalNumber)+")";
             stmt.executeUpdate(sqlQuery1);
 
-            String sqlQuery2 = operAccTypeOrg(number);
+            String sqlQuery2 = operAccTypeOrg(number, numOrg);
             stmt.executeUpdate(sqlQuery2);
+            mdbc.close(stmt);
+            return 1;
 
         } catch (SQLException e) {
             e.printStackTrace();
+            return -1;
         }
     }
 
-    private String operAccTypeOrg(String operAccNumber) throws SQLException {
+    private String operAccTypeOrg(String operAccNumber, String num) throws SQLException {
         String code="";
         String sqlQuery="";
-        switch (getOperAccOrg()){
-            case 1:
-                ResultSet rs1 = stmt.executeQuery("select po.postCode from post_organization po");
-                if(rs1.next()){
-                    int postCode = rs1.getInt("postCode");
-                    code = String.valueOf(postCode);
-                    sqlQuery = "insert into post_operating_account(numberOperatingAccount, postCode) VALUES ("+quotate(operAccNumber)+","+quotate(code)+")";
-                }
-                break;
-            case 2:
-                ResultSet rs2 = stmt.executeQuery("select bo.bic from bank_organization bo");
-                if(rs2.next())
-                    code = rs2.getString("bic");
-                sqlQuery = "insert into bank_operating_account(numberOperatingAccount, bic) VALUES ("+quotate(operAccNumber)+","+quotate(code)+")";
-                break;
-            case 3:
-                ResultSet rs3 = stmt.executeQuery("select co.tinCash from cash_organization co");
-                if(rs3.next())
+        ResultSet rs1 = stmt.executeQuery("select po.postCode from post_organization po where po.postCode = '"+num+"'");
+        if(rs1.next()){
+            int postCode = rs1.getInt("postCode");
+            code = String.valueOf(postCode);
+            sqlQuery = "insert into post_operating_account(numberOperatingAccount, postCode) VALUES ("+quotate(operAccNumber)+","+quotate(code)+")";
+        }else{
+            ResultSet rs2 = stmt.executeQuery("select bo.bic from bank_organization bo where bo.bic = '"+num+"'");
+            if(rs2.next()) {
+                code = rs2.getString("bic");
+                sqlQuery = "insert into bank_operating_account(numberOperatingAccount, bic) VALUES (" + quotate(operAccNumber) + "," + quotate(code) + ")";
+            }else {
+                ResultSet rs3 = stmt.executeQuery("select co.tinCash from cash_organization co where co.tinCash = '" + num + "'");
+                if (rs3.next()) {
                     code = rs3.getString("tinCash");
-                sqlQuery = "insert into cash_operating_account(numberOperatingAccount, tinCash) VALUES ("+quotate(operAccNumber)+","+quotate(code)+")";
-                break;
+                    sqlQuery = "insert into cash_operating_account(numberOperatingAccount, tinCash) VALUES (" + quotate(operAccNumber) + "," + quotate(code) + ")";
+                }
+            }
         }
         return sqlQuery;
     }
 
-    private int getOperAccOrg(){
-        return textOrgComboBox.getSelectedIndex()+1;
-    }
     private String quotate(String content){
         return "'"+content+"'";
+    }
+
+    private void getComboBoxOrganization(JComboBox cb, JTable b, JTable p, JTable c){
+        cb.removeAllItems();
+        String item;
+        for (int i = 0;i<b.getRowCount();i++){
+            item = b.getModel().getValueAt(i, 0).toString()+" | "+b.getModel().getValueAt(i,2).toString();
+            cb.addItem(item);
+        }
+        for (int i = 0;i<p.getRowCount();i++){
+            item = p.getModel().getValueAt(i, 0).toString()+" | "+p.getModel().getValueAt(i,3).toString();
+            cb.addItem(item);
+        }
+        for (int i = 0;i<c.getRowCount();i++){
+            item = c.getModel().getValueAt(i, 0).toString()+" | "+c.getModel().getValueAt(i,2).toString();
+            cb.addItem(item);
+        }
     }
 }
